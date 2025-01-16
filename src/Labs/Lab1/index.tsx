73458f6def3c6bdc01d2eef88c352c9c77aeec79
alanluo <luo.yiju@northeastern.edu>
2025-01-15 23:52:49 -0500

    1.2.7 Creating Web Forms update

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,22 +1,22 @@
 export default function Lab1() {
-    return (
-      <div id="wd-lab1">
+  return (
+    <div id="wd-lab1">
       <h2>Lab 1</h2>
       <h3>HTML Examples</h3>
       <div id="wd-h-tag">
         <h4>Heading Tags</h4>
-        Text documents are often broken up into several sections and subsections. Each section is usually prefaced with a short title or 
-        heading that attempts to summarize the topic of the section it precedes. For instance this paragraph is preceded by the heading 
-        Heading Tags. The font of the section headings are usually larger and bolder than their subsection headings. This document uses 
-        headings to introduce topics such as HTML Documents, HTML Tags, Heading Tags, etc. HTML heading tags can be used to format plain 
-        text so that it renders in a browser as large headings. There are 6 heading tags for different sizes: h1, h2, h3, h4, h5, and h6. 
+        Text documents are often broken up into several sections and subsections. Each section is usually prefaced with a short title or
+        heading that attempts to summarize the topic of the section it precedes. For instance this paragraph is preceded by the heading
+        Heading Tags. The font of the section headings are usually larger and bolder than their subsection headings. This document uses
+        headings to introduce topics such as HTML Documents, HTML Tags, Heading Tags, etc. HTML heading tags can be used to format plain
+        text so that it renders in a browser as large headings. There are 6 heading tags for different sizes: h1, h2, h3, h4, h5, and h6.
         Tag h1 is the largest heading and h6 is the smallest heading.
       </div>
       <div id="wd-p-tag">
         <h4>Paragraph Tag</h4>
         <p id="wd-p-1">
-          This is a paragraph. We often separate a long set of sentences with vertical spaces to make the text easier to read. Browsers 
-          ignore vertical white spaces and render all the text as one single set of sentences. To force the browser to add vertical spacing, 
+          This is a paragraph. We often separate a long set of sentences with vertical spaces to make the text easier to read. Browsers
+          ignore vertical white spaces and render all the text as one single set of sentences. To force the browser to add vertical spacing,
           wrap the paragraphs you want to separate with the paragraph tag
         </p>
 
@@ -24,7 +24,7 @@ export default function Lab1() {
           This is the first paragraph. The paragraph tag is used to format vertical gaps between long pieces of text like this one.
         </p>
         <p id="wd-p-3">
-          This is the second paragraph. Even though there is a deliberate white gap between the paragraph above and this paragraph, by 
+          This is the second paragraph. Even though there is a deliberate white gap between the paragraph above and this paragraph, by
           default browsers render them as one contiguous piece of text as shown here on the right.
         </p>
         <p id="wd-p-4">
@@ -62,13 +62,97 @@ export default function Lab1() {
             <li>Red Mars</li>
             <li>The Forever War</li>
           </ul>
-
-
-      </div>
+          Your favorite books (in no particular order)
+          <ul id="wd-your-books">
+            <li>To Kill a Mockingbird</li>
+            <li>1984</li>
+            <li>Harry Potter and the Sorcerer's Stone</li>
+          </ul>
+        </div>
+        <div id="wd-tables">
+          <h4>Table Tag</h4>
+          <table border={1} width="100%">
+            <thead>
+              <tr>
+                <th>Quiz</th>
+                <th>Topic</th>
+                <th>Date</th>
+                <th>Grade</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Q1</td>
+                <td>HTML</td>
+                <td>2/3/21</td>
+                <td>85</td>
+              </tr>
+              <tr>
+                <td>Q2</td>
+                <td>CSS</td>
+                <td>2/10/21</td>
+                <td>90</td>
+              </tr>
+              <tr>
+                <td>Q3</td>
+                <td>JavaScript</td>
+                <td>2/17/21</td>
+                <td>88</td>
+              </tr>
+              <tr>
+                <td>Q4</td>
+                <td>React</td>
+                <td>2/24/21</td>
+                <td>92</td>
+              </tr>
+              <tr>
+                <td>Q5</td>
+                <td>Node.js</td>
+                <td>3/3/21</td>
+                <td>87</td>
+              </tr>
+              <tr>
+                <td>Q6</td>
+                <td>Express</td>
+                <td>3/10/21</td>
+                <td>89</td>
+              </tr>
+              <tr>
+                <td>Q7</td>
+                <td>MongoDB</td>
+                <td>3/17/21</td>
+                <td>91</td>
+              </tr>
+              <tr>
+                <td>Q8</td>
+                <td>TypeScript</td>
+                <td>3/24/21</td>
+                <td>86</td>
+              </tr>
+              <tr>
+                <td>Q9</td>
+                <td>Redux</td>
+                <td>3/31/21</td>
+                <td>93</td>
+              </tr>
+              <tr>
+                <td>Q10</td>
+                <td>Testing</td>
+                <td>4/7/21</td>
+                <td>90</td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colSpan={3}>Average</td>
+                <td>90</td>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
 
       </div>
     </div>
 
-    );
-  }
-  
+  );
+}
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -6,6 +6,18 @@ export default function Labs() {
       <h1>Labs</h1>
       <Lab1 />
       <Lab2 />
+      <div id="wd-images">
+        <h4>Image tag</h4>
+        Loading an image from the internet: <br />
+        <img id="wd-starship" width="400px"
+          src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" />
+        <br />
+        Loading a local image:
+        <br />
+        <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" />
+      </div>
+
     </div>
+
   );
 }
