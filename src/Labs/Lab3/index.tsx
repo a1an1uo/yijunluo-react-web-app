47f2b241944de9e0f55a9ddf0125660361c396c1
alanluo <luo.yiju@northeastern.edu>
2025-02-12 03:29:03 -0500

    3.2

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,5 +1,11 @@
+import ArrowFunctions from "./ArrowFunctions";
 import BooleanVariables from "./BooleanVariables";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
 import IfElse from "./IfElse";
+import ImpliedReturn from "./ImpliedReturn";
+import LegacyFunctions from "./LegacyFunctions";
+import TemplateLiterals from "./TemplateLiterals";
 import TernaryOperator from "./TernaryOperator";
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
@@ -13,6 +19,12 @@ export default function Lab3() {
       <BooleanVariables/>
       <IfElse/>
       <TernaryOperator/>
+      <ConditionalOutputIfElse/>  
+      <ConditionalOutputInline/>
+      <LegacyFunctions/>
+      <ArrowFunctions/>
+      <ImpliedReturn/>
+      <TemplateLiterals/>
     </div>
   );
 }
